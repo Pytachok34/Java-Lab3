75112a7ebd21b21e37d6736eca3fcabd841a34da
Pytachok34 <andrey.abdurakhimov@bk.ru>
2024-05-26 15:40:56 +0300

    Add comment, changed test Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Timer tester = new Timer(100000);
+
+        System.out.println(tester.testAll());
     }
 }
--- src/Timer.java
+++ src/Timer.java
@@ -1,17 +1,33 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-
+/**
+ * класс таймер, позволяющий сравнивать временную эффеткивность arrayList и linkedList
+ * для таких операций как remove, get, set, add
+ * сравнине происходит на amount итерациях
+ */
 class Timer {
-    final private List<Integer> arrayList;
-    final private List<Integer> linkedList;
+    /** Реализация списка через arrayList, для последующего сравнения эффективности*/
+    private List<Integer> arrayList;
+    /** Реализация списка через linkedList, для последующего сравнения эффективности*/
+    private List<Integer> linkedList;
+    /** Количество итераций для засекания времени*/
     final private Integer amount;
+    /**
+     * Конструктор класса, в нем происходит инициализация двух реализация списка
+     * и количества итераций
+     * @param amount количество итераций, заданное пользователем
+     */
     public Timer(final int amount) {
         this.amount = amount;
         this.arrayList = new ArrayList();
         this.linkedList = new LinkedList();
     }
-
+    /**
+     * Метод поочередно запускающий тесты для каждой операции
+     * сохраняющий результаты тестирования в общую строковую переменную
+     * @return результаты четырёх проведенных временнЫх тестов
+     */
     final public String testAll() {
         String res = "";
         res += testOperation("remove") + "\n";
@@ -21,11 +37,20 @@ class Timer {
 
         return res;
     }
-
+    /**
+     * Универсальный метод, позволяющий засечь время для arrayList и linkedList
+     * в любой из выбранных операций (remove, get, set, add)
+     * если заданной операции нет в списке достуных, вызываем ошибку времени выполнения
+     * для remove get set списки очищаются и инициализируются рандомныи значениями
+     * для add списки реинициализируются
+     * при операции remove удалиние происходит из середины списка
+     * @param operation операция для которой засекаем время выполнения
+     * @return строка содержащая информацию о результатах замеров времени
+     */
     final public String testOperation(String operation) {
         switch (operation) {
             case "remove", "get", "set" -> fillArrays();
-            case "add" -> clearArrays();
+            case "add" -> reinitArrays();
             default -> throw new RuntimeException("Invalid operation");
         }
 
@@ -37,12 +62,9 @@ class Timer {
         for(int i = 0; i < amount; i++) {
             switch (operation) {
                 case "remove" -> arrayList.remove(arrayList.size() / 2);
-                case "add" -> {
-                    clearArrays();
-                    arrayList.add(i);
-                }
+                case "add" -> arrayList.add(i);
                 case "get" -> {
-                    int tmp = arrayList.get(arrayList.size() / 2);
+                    int tmp = arrayList.get(i);
                 }
                 case "set" -> arrayList.set(i, i - 356);
             }
@@ -53,12 +75,9 @@ class Timer {
         for(int i = 0; i < amount; i++) {
             switch (operation) {
                 case "remove" -> linkedList.remove(linkedList.size() / 2);
-                case "add" -> {
-                    clearArrays();
-                    linkedList.add(i);
-                }
+                case "add" -> linkedList.add(i);
                 case "get" -> {
-                    int tmp = linkedList.get(linkedList.size() / 2);
+                    int tmp = linkedList.get(i);
                 }
                 case "set" -> linkedList.set(i, i - 356);
             }
@@ -72,24 +91,24 @@ class Timer {
     private void fillArrays() {
         Integer tmp;
 
-        clearArrays();
+        reinitArrays();
 
-        for(int i=0;i<amount;i++) {
-            tmp = (int) (Math.random() * (1000+100)) + 1000;
+        for(int i = 0; i < amount; i++) {
+            tmp = (int) (Math.random() * (1000 + 100)) + 1000;
             arrayList.add(tmp);
             linkedList.add(tmp);
         }
     }
 
-    private void clearArrays() {
-        if(!arrayList.isEmpty() || !linkedList.isEmpty()) {
-            arrayList.clear();
-            linkedList.clear();
-        }
-    }
 
     public int getListsSize() {
         return arrayList.size();
     }
-
+    /**
+     * Функция реинициализирующая списки
+     */
+    private void reinitArrays() {
+        arrayList = new ArrayList();
+        linkedList = new LinkedList();
+    }
 }
